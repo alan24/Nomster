69c001a208850b6dee6f911cbd2e991d8a645eb2
Alan <alanlim25@gmail.com>
2015-09-24 20:05:28 +0000

    Edit button now works

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,8 +2,7 @@ class PlacesController < ApplicationController
 	before_action :authenticate_user!, :only => [:new, :create]
 	
 	def index
-		@places = Place.all
-		@places = Place.paginate(:page => params[:page], :per_page => 1)
+		@places = Place.all.paginate(:page => params[:page], :per_page => 1).order("created_at ASC")
 	end
 
 	def new
@@ -21,6 +20,12 @@ class PlacesController < ApplicationController
 
 	def edit
 		@place = Place.find(params[:id])
+	end
+
+	def update
+		@place = Place.find(params[:id])
+		@place.update_attributes(place_params)
+		redirect_to root_path
 	end
 
 	private
