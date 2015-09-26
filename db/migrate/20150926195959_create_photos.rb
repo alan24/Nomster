e3b0512df09b16d22b6e51a71af4cc9a171378de
Alan <alanlim25@gmail.com>
2015-09-26 20:01:57 +0000

    Attempting to fix migrate

--- db/migrate/20150926195959_create_photos.rb
+++ db/migrate/20150926195959_create_photos.rb
@@ -3,10 +3,8 @@ class CreatePhotos < ActiveRecord::Migration
     create_table :photos do |t|
     	t.text :caption
     	t.integer :place_id
-      	
+
       	t.timestamps
     end
-
-    add_index :photos, :place_id
   end
 end
